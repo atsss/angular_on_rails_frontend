907e79ef80924f3d7f1faeab796770b6da303338
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-02-24 09:29:33 +0900

    create http function in question service

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Question } from 'app/question';
 
 @Injectable()
 export class QuestionService {
+  private questionsUrl = 'questions';  // URL to web api
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposees only
+    return Promise.reject(error.message || error);
+  }
+  private headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getQuestiones(): Promise<Question[]> {
+    return this.http.get(this.questionsUrl)
+               .toPromise()
+               .then(response => response.json().data as Question[])
+               .catch(this.handleError);
+  }
+
+  getQuestion(id: number): Promise<Question> {
+    return this.getQuestiones().then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  create(name: string): Promise<Question> {
+    return this.http
+      .post(this.questionsUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json().data)
+      .catch(this.handleError);
+  }
 }
