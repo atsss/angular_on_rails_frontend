20705336edb3eeea3d0eef00e95b779e32e7677f
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-01 09:44:46 +0900

    create new page of question detail

--- src/app/question-detail/question-detail.component.ts
+++ src/app/question-detail/question-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { Question } from 'app/question';
+import { QuestionService } from 'app/question.service';
 
 @Component({
   selector: 'app-question-detail',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionDetailComponent implements OnInit {
 
-  constructor() { }
+  question: Question;
 
-  ngOnInit() {
+  constructor(
+    private questionService: QuestionService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.questionService.getQuestion(+params['id']))
+      .subscribe(question => this.question = question);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
