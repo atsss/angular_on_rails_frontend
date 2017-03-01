03370cbf692aad46461654c08264d226ebd352fd
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-01 09:44:00 +0900

    set routing for QuestionDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { QuestionIndexComponent }   from './question-index/question-index.component';
+import { QuestionDetailComponent }   from './question-detail/question-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/questions', pathMatch: 'full' },
   { path: 'questions',  component: QuestionIndexComponent },
+  { path: 'questions/:id',  component: QuestionDetailComponent },
 ];
 
 @NgModule({
