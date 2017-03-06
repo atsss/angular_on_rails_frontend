d64f85a098777b80559e553b86bf3e43e258ab59
atsushi0623itoh <atsushi0623itoh@gmail.com>
2017-03-07 00:30:30 +0900

    display NewQuestionComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { QuestionIndexComponent }   from './question-index/question-index.component';
 import { QuestionDetailComponent }   from './question-detail/question-detail.component';
+import { NewQuestionComponent }   from './new-question/new-question.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/questions', pathMatch: 'full' },
   { path: 'questions',  component: QuestionIndexComponent },
   { path: 'questions/:id',  component: QuestionDetailComponent },
+  { path: 'new',  component: NewQuestionComponent }
 ];
 
 @NgModule({
